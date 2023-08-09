bbf9526f2a79c0604dcfae26e83958597c799611
Kokchun Giang <kokchungiang@gmail.com>
2023-08-09 15:21:46 +0200

    some styling

--- src/app.py
+++ src/app.py
@@ -16,4 +16,4 @@ app.layout = layout
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(debug = False, host = "0.0.0.0", port = 443)
+    app.run_server(debug = True, host = "0.0.0.0", port = 443)
